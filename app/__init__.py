1551c35efd894296da5b0d574ce847e5cf6abbc0
Plusx <a147896325811@gmail.com>
2024-11-21 13:04:00 +0800

    add CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from app.routes import upload
-
+from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
-    
+    CORS(app)
     # 加載配置
     app.config.from_object("app.config.Config")
     
